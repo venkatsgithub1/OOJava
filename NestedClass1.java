8485da328481e66be4017a92bbc39e6e76f8281b
venkatsgithub1 <venkatsgithub@gmail.com>
2017-04-25 23:32:16 +0530

    Update NestedClass1.java

--- NestedClass1.java
+++ NestedClass1.java
@@ -16,6 +16,9 @@ public class NestedClass1 {
 		NestedClass1 nc1 = new NestedClass1();
 		NestedClass1.Inner inner = nc1.new Inner();
 		inner.print();
+
+		Outer outer = new Outer();
+		outer.logAMessage();
 	}
 
 	/**
@@ -33,5 +36,31 @@ public class NestedClass1 {
 		public void print() {
 			System.out.println("I am a method in the inner class");
 		}
+	}
+}
+
+/**
+ * A foreign class created to access the inner class of the public class.
+ * 
+ * @author Venkat.
+ *
+ */
+class Outer {
+	/**
+	 * The below code can be used in a method too. Used it straight away in a
+	 * constructor so that on instantiation of the class inner class access can
+	 * be seen.
+	 */
+	Outer() {
+		NestedClass1 nc1 = new NestedClass1();
+		NestedClass1.Inner innerInst = nc1.new Inner();
+		innerInst.print();
+	}
+
+	/**
+	 * A void method created to log a message.
+	 */
+	public void logAMessage() {
+		System.out.println("I am able to access the inner class of another class :)");
 	}
 }
