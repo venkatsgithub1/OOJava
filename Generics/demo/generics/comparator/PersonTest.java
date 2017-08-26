d02cae064460121bacb39c86da946d0255dbebae
venkatsgithub1 <venkatsgithub@gmail.com>
2017-08-26 13:12:53 +0530

    Update PersonTest.java

--- Generics/demo/generics/comparator/PersonTest.java
+++ Generics/demo/generics/comparator/PersonTest.java
@@ -2,6 +2,7 @@ package demo.generics.comparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class PersonTest {
@@ -26,5 +27,30 @@ public class PersonTest {
 		Collections.sort(personList, new ReverseComparatorClass<>(new AgeComparator()));
 		System.out.println("Descending order by age");
 		System.out.println(personList);
+
+		System.out.println(min(personList, new AgeComparator()));
+	}
+
+	/**
+	 * The method is used to determine
+	 * a minimum value from the list of
+	 * values.
+	 * 
+	 * @param listVals
+	 * @param comparator
+	 * @return
+	 */
+	public static <T> Object min(List<T> listVals, Comparator<T> comparator) {
+		if (listVals.isEmpty()) {
+			throw new IllegalArgumentException("List is empty");
+		}
+		T lowest = listVals.get(0);
+		T element = null;
+		for (int i = 1; i < listVals.size(); i++) {
+			element = listVals.get(i);
+			if (comparator.compare(element, lowest) < 0)
+				lowest = element;
+		}
+		return lowest;
 	}
 }
